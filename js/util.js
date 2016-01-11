367b131883899642e42b5c95d534ba62aec2ff4b
Oliver Heilmann <oliver@heilmann.co.uk>
2016-01-11 01:23:20 +0000

    sorting thing

--- js/util.js
+++ js/util.js
@@ -10,3 +10,25 @@ function repeatArray(newSize, inArray) {
 
     return ret;
 }
+
+
+var arr = [{"a":"1", "b":"cat", "c":"2"},{"a":"10", "b":"abc", "c":"2"}, {"a":"1000", "b":"dog", "c":"1"}];
+
+
+function xxx(o1, o2) {
+    for(var i=0; i<this.length; ++i) {
+        if(o1[this[i]]>o2[this[i]]) return 1;
+        if(o1[this[i]]<o2[this[i]]) return -1;
+    }
+    return 0;
+}
+
+
+arr.sort(xxx.bind(["b","a","c"]));
+
+console.log("" + JSON.stringify(arr));
+
+arr.sort(xxx.bind(["c"]));
+
+
+console.log("" + JSON.stringify(arr));
